54d269d3c4a9e5e6b26c7cedd5b3eb5963cff1f0
Mateusz Matoszko <mateusz@matoszko.pl>
2018-12-24 00:18:40 +0100

    Initial commit

---
+++ RandomUser/UserListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomUser
+//
+//  Created by Mateusz Matoszko on 21/12/2018.
+//  Copyright © 2018 Mateusz Matoszko. All rights reserved.
+//
+
+import UIKit
+
+class UserListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
