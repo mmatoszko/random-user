d746d8db83891be2f582e4fc352834b7cc7cf22c
Mateusz Matoszko <mateusz@matoszko.pl>
2018-12-24 17:26:28 +0100

    Add empty user model

---
+++ RandomUser/Model/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  RandomUser
+//
+//  Created by Mateusz Matoszko on 24/12/2018.
+//  Copyright © 2018 Mateusz Matoszko. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+}
