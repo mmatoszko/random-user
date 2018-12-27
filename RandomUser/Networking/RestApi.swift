98290bd5188263f44ebf204df2d30007f6dd76db
Mateusz Matoszko <mateusz@matoszko.pl>
2018-12-27 14:07:52 +0100

    Get 150 users in RestApi

--- RandomUser/Networking/RestApi.swift
+++ RandomUser/Networking/RestApi.swift
@@ -13,7 +13,7 @@ import RxCocoa
 
 final class RestApi {
 
-    private let apiUrl = URL(string: "https://randomuser.me/api/?results=5")!
+    private let apiUrl = URL(string: "https://randomuser.me/api/?results=150")!
 
     private let urlSession: URLSession
 
@@ -24,6 +24,7 @@ final class RestApi {
     func getUsers() -> Observable<[User]> {
         let request = URLRequest(url: apiUrl)
         return urlSession.rx.data(request: request).map { data in
+
             let users = try JSONDecoder().decode(Users.self, from: data)
             return users.results
         }
