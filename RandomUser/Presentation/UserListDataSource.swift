140da0ad6c7ec135a69bb2977b09172a3d794f77
Mateusz Matoszko <mateusz@matoszko.pl>
2018-12-27 18:06:16 +0100

    Add UserListDataSource

---
+++ RandomUser/Presentation/UserListDataSource.swift
@@ -0,0 +1,12 @@
+//
+//  UserListDataSource.swift
+//  RandomUser
+//
+//  Created by Mateusz Matoszko on 27/12/2018.
+//  Copyright © 2018 Mateusz Matoszko. All rights reserved.
+//
+
+import UIKit
+
+final class UserListDataSource: NSObject {
+}
