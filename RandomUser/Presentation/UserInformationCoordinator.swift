71ee95c9e583abf90ff80212fa2d6d5d10c88563
Mateusz Matoszko <mateusz@matoszko.pl>
2018-12-27 17:21:01 +0100

    Feed userRepository into UserListViewController

--- RandomUser/Presentation/UserInformationCoordinator.swift
+++ RandomUser/Presentation/UserInformationCoordinator.swift
@@ -12,14 +12,36 @@ import UIKit
 final class UserInformationCoordinator {
 
     private let window: UIWindow
+    private let userStore: UserStore
 
     init(window: UIWindow) {
         self.window = window
+        self.userStore = createUserStore()
     }
 
     func start() {
-        let userListViewController = UserListViewController()
+        let userListViewController = UserListViewController(userRepository: userRepository)
         window.rootViewController = userListViewController
         window.makeKeyAndVisible()
     }
+
+    private lazy var userRepository: UserRepository = {
+        let restApi = createRestApi()
+        let userStore = createUserStore()
+        return UserRepository(restApi: restApi, userStore: userStore)
+    }()
+}
+
+private func createRestApi() -> RestApi {
+    let configuration = URLSessionConfiguration.default
+    let session = URLSession(configuration: configuration)
+    return RestApi(urlSession: session)
+}
+
+private func createUserStore() -> UserStore {
+    let store = UserStore(containerName: "RandomUser")
+    store.load { success in
+        assert(success, "User store should be properly created")
+    }
+    return store
 }
--- RandomUser/Presentation/UserListViewController.swift
+++ RandomUser/Presentation/UserListViewController.swift
@@ -7,11 +7,32 @@
 //
 
 import UIKit
+import RxSwift
 
 class UserListViewController: UIViewController {
 
+    let disposeBag = DisposeBag()
+
+    private var userRepository: UserRepository
+
+    init(userRepository: UserRepository) {
+        self.userRepository = userRepository
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        userRepository.getUsers()
+            .subscribe(onNext: { users in
+                print(users.count)
+
+            }).disposed(by: disposeBag)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
