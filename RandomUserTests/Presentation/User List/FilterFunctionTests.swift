c4b44daf6170537b31231dbc5ced1c645dcfa774
Mateusz Matoszko <mateusz@matoszko.pl>
2019-01-03 22:53:13 +0100

    Add tests for filterFunction

---
+++ RandomUserTests/Presentation/User List/FilterFunctionTests.swift
@@ -0,0 +1,50 @@
+//
+//  FilterFunctionTests.swift
+//  RandomUserTests
+//
+//  Created by Mateusz Matoszko on 03/01/2019.
+//  Copyright © 2019 Mateusz Matoszko. All rights reserved.
+//
+
+import XCTest
+@testable import RandomUser
+
+
+class FilterFunctionTests: XCTestCase {
+
+    func testFilteringWithEmptySearchText() {
+
+        let filterFunctionWithEmptyText: (Car) -> Bool = filterFunction(searchText: "")
+        XCTAssertEqual(14, testObjects.filter(filterFunctionWithEmptyText).count)
+    }
+
+    func testFilteringForLowercase() {
+        let filterFunctionWithEmptyText: (Car) -> Bool = filterFunction(searchText: "m")
+        XCTAssertEqual(5, testObjects.filter(filterFunctionWithEmptyText).count)
+    }
+
+    func testFilteringForUppercase() {
+        let filterFunctionWithEmptyText: (Car) -> Bool = filterFunction(searchText: "M")
+        XCTAssertEqual(5, testObjects.filter(filterFunctionWithEmptyText).count)
+    }
+}
+
+private struct Car: FullName {
+    var fullName: String
+}
+
+private let testObjects = [
+    "BMW",
+    "Volvo",
+    "Mazda",
+    "Mercedes-Benz",
+    "Koenigsegg",
+    "Porsche",
+    "Jaguar",
+    "Audi",
+    "Lincoln",
+    "GMC",
+    "Pagani",
+    "Subaru",
+    "Alfa Romeo",
+    "Acura"].map(Car.init)
