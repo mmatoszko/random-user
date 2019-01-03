2d86a61d38830164a186134c5c68f832e204dc4f
Mateusz Matoszko <mateusz@matoszko.pl>
2019-01-03 17:29:20 +0100

    Put user data into UserDetailsViewController

--- RandomUser/Presentation/User Details/UserDetailsViewController.swift
+++ RandomUser/Presentation/User Details/UserDetailsViewController.swift
@@ -26,6 +26,47 @@ class UserDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+
+        let views = createViews(for: user)
+        let stackView = createStackView(with: views)
+        view.addSubview(stackView)
+
+        let constrains = createConstraints(for: stackView, in: view)
+        view.addConstraints(constrains)
     }
 
+}
+
+private func createStackView(with views: [UIView]) -> UIStackView {
+    let stackView = UIStackView(arrangedSubviews: views)
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    stackView.axis = .vertical
+    stackView.alignment = .center
+    stackView.distribution = .equalCentering
+    stackView.spacing = 20
+    return stackView
+}
+
+private func createViews(for user: User) -> [UIView] {
+    let imageView = UIImageView()
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    imageView.kf.setImage(with: user.picture.large)
+
+    let userNameLabel = UILabel()
+    userNameLabel.translatesAutoresizingMaskIntoConstraints = false
+    let name = user.name
+    userNameLabel.text = "\(name.title) \(name.first) \(name.last)"
+
+    let emailLabel = UILabel()
+    emailLabel.translatesAutoresizingMaskIntoConstraints = false
+    emailLabel.text = user.email
+    return [imageView, userNameLabel, emailLabel]
+}
+
+private func createConstraints(for stackView: UIStackView, in view: UIView) -> [NSLayoutConstraint] {
+    let constrains = [
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 0),
+        ]
+    return constrains
 }
