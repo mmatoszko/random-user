4a8da17da5eb02c3f848612283f2944b51260be1
Mateusz Matoszko <mateusz@matoszko.pl>
2020-07-24 00:48:14 +0200

    Add base viper classes for user list

---
+++ RandomUser/Presentation/User List/UserListPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  UserListPresenter.swift
+//  RandomUser
+//
+//  Created by Mateusz Matoszko on 24/07/2020.
+//  Copyright © 2020 Mateusz Matoszko. All rights reserved.
+//
+
+import Foundation
+
+class UserListPresenter {
+
+}
