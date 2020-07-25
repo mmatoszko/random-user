a1742403d06f07fb9394d9d9a3e817dc659c7228
Mateusz Matoszko <mateusz@matoszko.pl>
2020-07-25 16:55:26 +0200

    Use UserListPresenter in UserListViewController

--- RandomUser/Presentation/User List/UserListDataSource.swift
+++ RandomUser/Presentation/User List/UserListDataSource.swift
@@ -10,46 +10,18 @@ import UIKit
 
 final class UserListDataSource: NSObject, UICollectionViewDataSource {
 
-    private var users: [User] = []
-    var visibleUsers: [User] = []
-
-    /** Presentation state of the DataSource. */
-    var presentationType: PresentationType = .normal {
-        didSet { reloadVisibleUsers(for: presentationType) }
-    }
-
-    func updateUsers(users: [User]) {
-        self.users = users
-        reloadVisibleUsers(for: presentationType)
-    }
+    var users: [User] = []
 
     // MARK: - UICollectionViewDataSource Delegate
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return visibleUsers.count
+        return users.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UserCollectionViewCell.identifier, for: indexPath) as! UserCollectionViewCell
-        let user = visibleUsers[indexPath.row]
+        let user = users[indexPath.row]
         cell.render(user: user)
         return cell
     }
-
-    // MARK: - Private Methods
-
-    private func reloadVisibleUsers(for presentationType: PresentationType) {
-        switch presentationType {
-        case .normal:
-            self.visibleUsers = users
-        case .filtered(let predicate):
-            self.visibleUsers = users.filter(predicate)
-        }
-    }
-}
-
-/** Representation of the presentation type used in the `UserListViewController`. */
-enum PresentationType {
-    case normal
-    case filtered(predicate: (User) -> Bool)
 }
--- RandomUser/Presentation/User List/UserListPresenter.swift
+++ RandomUser/Presentation/User List/UserListPresenter.swift
@@ -6,8 +6,53 @@
 //  Copyright © 2020 Mateusz Matoszko. All rights reserved.
 //
 
-import Foundation
+import RxCocoa
+import RxSwift
 
 class UserListPresenter {
 
+    typealias ReloadUsersLookup = ([User]) -> Void
+
+    var reloadUsersLookup: ReloadUsersLookup = { _ in }
+
+    var dataSource: UserListDataSource
+    var userListDelegate: UserListDelegate?
+
+    private let interactor: UserListInteractor
+    private let router: UserListRouter
+    private let userCount = 15
+
+    private let disposeBag = DisposeBag()
+
+    init(interactor: UserListInteractor, router: UserListRouter) {
+        self.interactor = interactor
+        self.router = router
+
+        dataSource = UserListDataSource()
+        userListDelegate = UserListDelegate(cellSelectionCallback: { [weak self] indexPath in
+            guard let user = self?.dataSource.users[indexPath.row] else {
+                assertionFailure("Can't get user at index \(indexPath.row)")
+                return
+            }
+//            self?.coordinator?.showUserDetails(user: user)
+        })
+        interactor.visibleUsers.asDriver()
+            .drive(onNext: { [weak self] users in
+                self?.dataSource.users = users
+                self?.reloadUsersLookup(users)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    func loadUsers() {
+        interactor.loadUsers(userCount: userCount)
+    }
+
+    func refreshUsers() {
+        interactor.refreshUsers(userCount: userCount)
+    }
+
+    func updateSearchResults(presentationType: PresentationType) {
+        interactor.presentationType.accept(presentationType)
+    }
 }
