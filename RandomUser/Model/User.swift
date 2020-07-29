f609d2509da2298f9d01ce62eb0ef959753adc1a
Mateusz Matoszko <mateusz@matoszko.pl>
2020-07-29 15:37:04 +0200

    Make User Hashable

--- RandomUser/Model/User.swift
+++ RandomUser/Model/User.swift
@@ -16,7 +16,8 @@ protocol FullName {
     var fullName: String { get }
 }
 
-struct User: Decodable, FullName {
+struct User: Decodable, Hashable, FullName {
+
     let name: Name
     let email: String
     let login: Login
@@ -40,5 +41,17 @@ struct User: Decodable, FullName {
 
     var fullName: String {
         return "\(name.title) \(name.first) \(name.last)"
+    }
+
+    // MARK: - Equatable
+
+    static func == (lhs: User, rhs: User) -> Bool {
+        return lhs.login.uuid == rhs.login.uuid
+    }
+
+    // MARK: - Hashable
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(login.uuid)
     }
 }
